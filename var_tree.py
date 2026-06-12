90a092fe3ca7aad939aa96326830df89a4fc02f7
Rayyan <m.rayyanshaji@gmail.com>
2026-06-13 00:16:36 +0530

    feat(dev-io_tree): variable tree node compleated

--- var_tree.py
+++ var_tree.py
@@ -5,6 +5,8 @@ import numpy as np # type: ignore
 
 # var node replace every var in token code
 class VariableFormation:
+    
+    # constructor of class
     def __init__(self, token_list: list[any], numeric_list: list[int]):
         self.token_list = token_list # dev
         self.numeric_list = numeric_list
@@ -20,18 +22,28 @@ class VariableFormation:
             if np.isin(def_skip, index).any():
                 loop_count+=1
                 continue
-               
-            # check if its a var by checking
+            
+            # check if its a var by checking if bef is = and after it have some value 
             elif index+1 < len(self.numeric_list) and i == 0 and self.numeric_list[index+1] == TokenType.EQUAL.value:
                 var_name = self.token_list[index]
                 variable_value = ''
                 loop_count = 0
                 skip_index = []
+                # loops from = until it reach a newline or list index out of range
                 while loop_count+index < len(self.numeric_list) and self.numeric_list[loop_count+index] != TokenType.NEWLINE.value :
+                    # skip index check for optimized code
                     if np.isin(skip_index, index+loop_count).any():
                         loop_count+=1
                         continue
                     
+                    # normal var to var match case
+                    elif loop_count+index+2 < len(self.numeric_list) and self.numeric_list[loop_count+index+2] == 0 and self.numeric_list[loop_count+index+1] == TokenType.EQUAL.value:
+                        self.token_list[loop_count+index+2] = self.token_list[loop_count+index+2].replace(' ','')
+                        value = get_memory(self.token_list[loop_count+index+2])
+                        variable_value+=value
+                        skip_index.extend(generate_index(index+loop_count, index+loop_count+2))
+                    
+                    # formatted string match case
                     elif self.numeric_list[loop_count+index] == TokenType.CURLY_BRACE_OPEN.value:
                         result = get_memory(self.token_list[loop_count+index+1]) if loop_count+index < len(self.numeric_list) else None
                         if result != None:
@@ -39,21 +51,27 @@ class VariableFormation:
                             loop_count+=1
                             skip_index.extend(generate_index(index+loop_count, index+loop_count+2))
                             continue
-                        
+                    
+                    # normal string match case
                     elif self.numeric_list[index+loop_count-1] == TokenType.QUOTE.value and i == 0:
                         variable_value += self.token_list[index+loop_count]
                     
+                    # formatted string normal text add case
                     elif index > 0 and self.token_list[index+loop_count-1] == TokenType.FORMAT.name and i == 0:
                         variable_value += self.token_list[index+loop_count]
-                        
+                    
+                    # formatted string after var text add case
                     elif index > 0 and self.numeric_list[index+loop_count-1] == TokenType.CURLY_BRACE_CLOSE.value and i == 0:
                         if self.numeric_list[index+loop_count] == 0:
                             variable_value += self.token_list[index+loop_count]
-                        
+                       
+                    # formatting var name to remove un necessary space 
                     var_name = var_name.replace(' ','')
                     set_memory(var_name, variable_value)
                     
+                    # incrementing loop
                     loop_count+=1
-                    
+                
+                # adding current skip index to for loop skip index too
                 def_skip.extend(skip_index)
                 
