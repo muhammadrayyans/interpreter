6b44197561fa8e3ac3b8d83c131c904f7a4f4a90
Rayyan <m.rayyanshaji@gmail.com>
2026-06-23 23:10:49 +0530

    refactor(setup): path re linked of condition validators

--- src/modules/setup.py
+++ src/modules/setup.py
@@ -3,15 +3,15 @@ from Cython.Build import cythonize
 import os
 
 setup( 
-      ext_modules=cythonize(['../condition_tree/condition_extractor.pyx',
-                             '../condition_tree/condition_parser.pyx',
-                             '../condition_tree/condition_scope.pyx',
-                             '../condition_tree/evaluator_node.pyx',
+      ext_modules=cythonize(['../condition_tree/condition_parser.pyx',
                              '../condition_tree/condition_tree.pyx',
                              '../config/variable_tree_config.pyx',
                              '../config/tokenization_config.pyx', 
                              '../library/maths/calculation.pyx',
+                             '../utils/condition_extractor.pyx',
                              '../io_tree/display_parser.pyx',
+                             '../utils/condition_scope.pyx',
+                             '../utils/evaluator_node.pyx',
                              '../io_tree/output_tree.pyx',
                              '../data_node/data_node.pyx',
                              '../io_tree/get_parser.pyx',
